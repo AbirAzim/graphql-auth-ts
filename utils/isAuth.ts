edfcea894d3c7bb5a6f4d779fd67e30b76af1306
AbirAzim <azim35-1713@diu.edu.bd>
2021-07-01 10:31:21 +0600

    is auth changes

--- utils/isAuth.ts
+++ utils/isAuth.ts
@@ -7,44 +7,7 @@ import catchAsync from "./catchAsync";
 
 export default catchAsync(
 	async (req: Request, res: Response, next: NextFunction) => {
-		// 1) Getting token and check of it's there
-    // console.log("here");
-		// let token;
-		// if (req.headers.authorization) {
-		// 	token = req.headers.authorization.split(" ")[1];
-		// }
-
-		// if (!token) {
-		// 	return next(
-		// 		new AppError(
-		// 			"You are not logged in! Please log in to get access.",
-		// 			401
-		// 		)
-		// 	);
-		// }
-
-		// const decoded: any = await promisify(jwt.verify)(
-		// 	token,
-		// 	// @ts-ignore
-		// 	process.env.JWT_SECRET
-		// );
-
-		// // 3) Check if user still exists
-		// const currentUser = await User.findById(decoded.id);
-		// if (!currentUser) {
-		// 	return next(
-		// 		new AppError(
-		// 			"The user belonging to this token does no longer exist.",
-		// 			401
-		// 		)
-		// 	);
-		// }
-
-		// // GRANT ACCESS TO PROTECTED ROUTE
-		// // @ts-ignore
-		// req.user = currentUser;
-		// res.locals.user = currentUser;
-		// next();
+		
     const authHeader = req.get("Authorization");
 
     if(!authHeader) {
